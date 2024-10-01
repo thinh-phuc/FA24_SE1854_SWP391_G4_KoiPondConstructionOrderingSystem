936595b999ea8a855f0227c0c2e61774393743a1
thinh-phuc <thinhthpse183083@fpt.edu.vn>
2024-10-01 14:33:53 +0700

    Add construction_history, acceptance_document entities

---
+++ Sourcecode/FA24_SE1854_SWP391_G4_KoiPondConstructionOrderingSystem/src/main/java/com/example/g4/FA24_SE1854_SWP391_G4_KoiPondConstructionOrderingSystem/entity/ConstructionHistory.java
@@ -0,0 +1,49 @@
+package com.example.g4.FA24_SE1854_SWP391_G4_KoiPondConstructionOrderingSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class ConstructionHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "construction_history_id")
+    private int constructionHistoryId;
+
+    @Column(name = "start_date", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime startDate = LocalDateTime.now();
+
+    @Column(name = "end_date")
+    private LocalDateTime endDate;
+
+    @Column(name = "step", columnDefinition = "NVARCHAR(80)")
+    private String step = "none";
+
+    @Column(name = "description", columnDefinition = "NVARCHAR(500)")
+    private String description = "none";
+
+    @Column(name = "note", columnDefinition = "NVARCHAR(255)")
+    private String note = "none";
+
+    @Column(name = "is_active", columnDefinition = "BIT DEFAULT 1")
+    private Boolean isActive = true;
+
+    @Column(name = "create_date", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createDate = LocalDateTime.now();
+
+    @Column(name = "create_by", columnDefinition = "NVARCHAR(100)")
+    private String createBy = "none";
+
+    @Column(name = "update_date", columnDefinition = "DATETIME DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime updateDate = LocalDateTime.now();
+
+    @Column(name = "update_by", columnDefinition = "NVARCHAR(100)")
+    private String updateBy = "none";
+}
